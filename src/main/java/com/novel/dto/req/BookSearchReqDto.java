ec5f94d81658e103ec93baa8f05a37c9c0071e0f
KittyBull <L6060909@163.com>
2026-05-06 22:40:23 +0800

    优化已基本实现！！！待下次云服务器docker部署时，使用 Docker Compose 一键安装开发环境（尚未创建 .env 文件->用来设置容器编排的环境变量。）--（先不替换目前服务器的项目）

--- src/main/java/com/novel/dto/req/BookSearchReqDto.java
+++ src/main/java/com/novel/dto/req/BookSearchReqDto.java
@@ -2,11 +2,9 @@ package com.novel.dto.req;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.novel.core.common.req.PageReqDto;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
